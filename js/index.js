f4602d233788f97ac9985d2da47d7622d16e16f2
UneebBhatti <88506513+Uneebbhat@users.noreply.github.com>
2022-05-06 17:36:44 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
     console.log('clicked');
-})
+});
+
+show = () => {
+    document.getElementById('change').innerHTML = '<b>web developer</b>';
+}
+
+show2 = () => {
+    document.getElementById('change').innerHTML = '<b>graphic designer</b>';
+}
+
+setInterval(show, 2000);
+setInterval(show2, 4000);
+
+
+comp = () => {
+    document.getElementById('comp').innerHTML = '<b>Service Shoes</b>';
+}
+
+comp2 = () => {
+    document.getElementById('comp').innerHTML = '<b>Wellcreator</b>';
+}
+
+setInterval(comp, 2000);
+setInterval(comp2, 4000);
